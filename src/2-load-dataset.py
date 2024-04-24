56475dca4abbfba6f4ae038ffb92bcbdb9f4bf7b
Gavin King <gavin.king@cgi.com>
2024-04-24 16:58:23 +0200

    Adding missing comments

--- src/2-load-dataset.py
+++ src/2-load-dataset.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
 
+# Load the MNIST dataset
 mnist = tf.keras.datasets.mnist
 
+# Load the dataset
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+# normalize the pixel values to be between 0 and 1
 x_train, x_test = x_train / 255.0, x_test / 255.0
